f8b49730eb21b6d3d7bd6208a520c552df51e2a9
Andrew Glebe <apglebe@gmail.com>
2024-02-02 15:14:18 -0800

    Minor formatting update. Moved odds to state. Next move odds to localStorage to get odds after games start.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,12 +8,18 @@ import Grid from '@mui/material/Grid';
 function App() {
 
    const [data, setData] = useState<any>([]);
+   const [oddsMap, setOddsMap] = useState(new Map());
+
    useEffect(() => {
       fetch('http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard')
          .then((response) => response.json())
          .then((data) => {
-            console.log(data.events);
             setData(data.events);
+              {data.events.map((event: any) => {
+                if(event.competitions[0].odds) {
+                    { setOddsMap(oddsMap.set(event.id, event.competitions[0].odds[0])) }
+                }
+              })}
          })
          .catch((err) => {
             console.log(err.message);
@@ -34,19 +40,25 @@ function App() {
                             <Card sx={{ minWidth: 400 }}>
                                 <CardContent>
                                     <p>{event.name} </p>
-                                    <p>Period/Quarter: {event.status.period}</p>
-                                    <p>Clock: {event.status.displayClock}</p>
+                                   <Grid container spacing={2}>
+                                    <Grid item xs={6}>
+                                      <p>Period/Quarter: {event.status.period}</p>
+                                      <p>Clock: {event.status.displayClock}</p>
                                     {event.competitions.map((comp: any) => {
                                         return (
                                             <div>
-                                                <p>Team: {comp.competitors[0].team.displayName} Score: {comp.competitors[0].score}</p>
-                                                <p>Team: {comp.competitors[1].team.displayName} Score: {comp.competitors[1].score}</p>
-                                                {comp.odds ?  <p>Spread: {comp.odds[0].spread}</p> : <p>Odds Not available</p>}
-                                                {comp.odds ?  <p>OverUnder: {comp.odds[0].overUnder}</p> : <p>Odds Not available</p>}
-                                                {comp.odds ?  <p>Details: {comp.odds[0].details}</p> : <p>Odds Not available</p>}
+                                                <p>{comp.competitors[0].team.abbreviation}: {comp.competitors[0].score}</p>
+                                                <p>{comp.competitors[1].team.abbreviation}: {comp.competitors[1].score}</p>
                                             </div>
                                         );
                                     })}
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <p>Spread: {oddsMap.get(event.id).spread}</p>
+                                        <p>OverUnder: {oddsMap.get(event.id).overUnder}</p>
+                                        <p>Details: {oddsMap.get(event.id).details}</p>
+                                    </Grid>
+                                   </Grid>
                                 </CardContent>
                             </Card>
                         </div>
